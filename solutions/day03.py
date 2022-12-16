f69540572af02552505af402bfa4d745b68b376d
Miho Krusic <miho.krusic@gmail.com>
2022-12-16 18:51:09 +0100

    Day 03

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,5 +1,38 @@
+import math
+
 def part1(input):
-    return 0
+    priority = 0
+    for line in input:
+        parsed = line.rstrip()
+        left = parsed[:math.trunc(len(parsed) / 2)]
+        right = parsed[math.trunc(len(parsed) / 2):]
+
+        left = set(list(left))
+        right = set(list(right))
+        intersection = left.intersection(right)
+        for e in intersection:
+            priority += __calc_priority(e)
+
+    return priority
 
 def part2(input):
+    priority = 0
+    for count, _ in enumerate(input):
+        if (count + 1) % 3 != 0:
+            continue
+
+        first = set(list(input[count - 2].rstrip()))
+        second = set(list(input[count - 1].rstrip()))
+        third = set(list(input[count].rstrip()))
+        intersection = first.intersection(second).intersection(third)
+        for e in intersection:
+            priority += __calc_priority(e)
+
+    return priority
+
+def __calc_priority(c):
+    if 65 <= ord(c) <= 90:
+        return ord(c) - 38
+    elif 97 <= ord(c) <= 122:
+        return ord(c) - 96
     return 0
--- tests/test03.py
+++ tests/test03.py
@@ -4,30 +4,30 @@ import os
 
 os.sys.path.insert(0, os.getcwd())
 
-from solutions import day02
+from solutions import day03
 from utility import inputs
 
 class Part1(unittest.TestCase):
     def test_01(self):
-        input = inputs.read("input02")
-        result = day02.part1(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input03")
+        result = day03.part1(input)
+        self.assertEqual(result, 157)
 
     def test_in(self):
-        input = inputs.read("input02_actual")
-        result = day02.part1(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input03_actual")
+        result = day03.part1(input)
+        self.assertEqual(result, 7674)
 
 class Part2(unittest.TestCase):
     def test_01(self):
-        input = inputs.read("input02")
-        result = day02.part2(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input03")
+        result = day03.part2(input)
+        self.assertEqual(result, 70)
 
     def test_in(self):
-        input = inputs.read("input02_actual")
-        result = day02.part2(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input03_actual")
+        result = day03.part2(input)
+        self.assertEqual(result, 2805)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
