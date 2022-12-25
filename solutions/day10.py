e889b068ac10ea8a9f459530cb4a51d83aedcdc2
Miho Krusic <miho.krusic@gmail.com>
2022-12-25 21:33:10 +0100

    Day 10

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,5 +1,56 @@
 def part1(input):
-    return 0
+    x = 1
+    cycle = 0
+    signal_strength = 0
+    check_signals = [[20, False], [60, False], [100, False], [140, False], [180, False], [220, False]]
+    for line in input:
+        command_arr = line.rstrip().split(' ')
+        x_to_add = 0
+        if command_arr[0] == 'noop':
+            cycle += 1
+        elif command_arr[0] == 'addx':
+            cycle += 2
+            x_to_add = int(command_arr[1])
+
+        for i, [target_cycle, reached] in enumerate(check_signals):
+            if not reached and cycle >= target_cycle:
+                check_signals[i][1] = True
+                signal_strength += (target_cycle * x)
+
+        x += x_to_add
+
+    return signal_strength
 
 def part2(input):
+    x = 1
+    cycle = 0
+    display = []
+    print('')
+    print('')
+    print('')
+    for _, line in enumerate(input):
+        command_arr = line.rstrip().split(' ')
+        x_to_add = 0
+        cycles_to_add = 0
+        if command_arr[0] == 'noop':
+            cycles_to_add = 1
+        elif command_arr[0] == 'addx':
+            cycles_to_add = 2
+            x_to_add = int(command_arr[1])
+
+        for c in range(cycles_to_add):
+            col = (cycle + c) % 40
+            if col == 0:
+                display.append([])
+
+            pixel = '#' if x - 1 <= col <= x + 1 else '.'
+            display[len(display) - 1].append(pixel)
+        cycle += cycles_to_add
+        x += x_to_add
+
+    print('')
+    for row in display:
+        print(''.join(row))
+    print('')
+
     return 0
--- tests/test10.py
+++ tests/test10.py
@@ -4,29 +4,29 @@ import os
 
 os.sys.path.insert(0, os.getcwd())
 
-from solutions import day02
+from solutions import day10
 from utility import inputs
 
 class Part1(unittest.TestCase):
     def test_01(self):
-        input = inputs.read("input02")
-        result = day02.part1(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input10")
+        result = day10.part1(input)
+        self.assertEqual(result, 13140)
 
     def test_in(self):
-        input = inputs.read("input02_actual")
-        result = day02.part1(input)
-        self.assertEqual(result, 0)
+        input = inputs.read("input10_actual")
+        result = day10.part1(input)
+        self.assertEqual(result, 10760)
 
 class Part2(unittest.TestCase):
     def test_01(self):
-        input = inputs.read("input02")
-        result = day02.part2(input)
+        input = inputs.read("input10")
+        result = day10.part2(input)
         self.assertEqual(result, 0)
 
     def test_in(self):
-        input = inputs.read("input02_actual")
-        result = day02.part2(input)
+        input = inputs.read("input10_actual")
+        result = day10.part2(input)
         self.assertEqual(result, 0)
 
 if __name__ == '__main__':
